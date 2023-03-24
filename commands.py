65e89b52151831c7635382ee0d696762d7ae3744
Blake Horwill - LT1 <horwillblake@gmail.com>
2023-03-24 00:20:23 +0000

    refactor

--- commands.py
+++ commands.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def commands(cm, user, overlay, crop):
-    command = cm.split(" ")
+    command = cm.split()
     match command[0]:
         case "hello":
             print("hello", user.settings["Farm_Name"]+"s")
@@ -14,21 +14,51 @@ def commands(cm, user, overlay, crop):
         case "water":
             overlay.opacity[1] = 100
             return "Plants watered"
+
         case "plant":
-            return
+            if command[2] in user.save_data["Inventory"].keys():
+                print("1")
+                user.save_data["Inventory"][command[2]] -= 1
+                if user.save_data["Inventory"][command[2]] == 0:
+                    print("2")
+                    del user.save_data["Inventory"][command[2]]
+
+                user.save_data["Crops"][command[2]]["Tile_id"] = [crop.info["Name"].index(command[1]), 0]
+                print([crop.info["Name"].index(command[1]), 0])
+
+            return "Planted" + command[1]
+
         case "inventory":
-            return
+            return user.save_data["Inventory"]
+
+        case "money":
+            return "money: " + str(user.save_data["Money"])
+
         case "shop":
             message = ""
             for i in range(crop.info["Crop_Num"]):
                 message = message + crop.info["Name"][i] + " : " + str(crop.info["Crops"][i]["Price"]) + "\n"
             return message
+
         case "buy":
             return
-        case "query":
-            return
+
         case "harvest":
-            return
+            try:
+                pos = command[1]
+                harvest = user.save_data["Crops"][pos]["Tile_id"]
+                print(harvest)
+                if (crop.info["Crops"][(harvest[0]-1)]["max_type"] - 1) == harvest[1]:
+                    user.save_data["Money"] = user.save_data["Money"] + crop.info["Crops"][harvest[0]-1]["Harvest"]
+
+                user.save_data["Crops"][pos]["Tile_id"] = [0, 0]
+
+                return "Crop harvested"
+
+            except:
+                return "Invalid args"
+
+
         case _:
             return str("Unknown command: " + cm)
 
